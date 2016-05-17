9a84d8e48fde3425666fdfed670a8f31945f7d11
jleluyer <jeremy.le.luyer@gmail.com>
2016-05-17 15:04:25 -0400

    Create transdecoder_predict.sh

---
+++ 00_scripts/utility_scripts/transdecoder_predict.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#$ -N trandecod_predict
+#$ -M userID
+#$ -m beas
+#$ -pe smp 1
+#$ -l h_vmem=20G
+#$ -l h_rt=20:00:00
+#$ -cwd
+#$ -S /bin/bash
+
+TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
+SCRIPT=$0
+NAME=$(basename $0)
+LOG_FOLDER="98_log_files"
+cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
+
+# Move to job submission directory
+cd $SGE_O_WORKDIR
+
+#Global variables
+INPUT="05_trinity_assembly/Trinity.fasta"
+
+./00_scripts/transdecoder_utils/#!/bin/bash
+#$ -N trimmomatic
+#$ -M userID
+#$ -m beas
+#$ -pe smp 8
+#$ -l h_vmem=20G
+#$ -l h_rt=20:00:00
+#$ -cwd
+#$ -S /bin/bash
+
+TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
+SCRIPT=$0
+NAME=$(basename $0)
+LOG_FOLDER="98_log_files"
+cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
+
+# Move to job submission directory
+cd $SGE_O_WORKDIR
+
+#Global variables
+INPUT="05_trinity_assembly/Trinity.fasta"
+
+./00_scripts/transdecoder_utils/TransDecoder.Predict -t $INPUT 2>&1 | tee 98_log_files/"$TIMESTAMP"_transdecoder_predict.log 
