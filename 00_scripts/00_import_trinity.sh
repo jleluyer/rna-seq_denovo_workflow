138aaf5cb0354f76b83e9ec02c42fbc0e671ff7b
jleluyer <jeremy.le.luyer@gmail.com>
2016-05-17 13:58:20 -0400

    Added transrate, transvestigator and transdecoder

--- 00_scripts/00_import_trinity.sh
+++ 00_scripts/00_import_trinity.sh
@@ -6,6 +6,14 @@ cd $(pwd)
 #clone into trinity
 git clone https://github.com/trinityrnaseq/trinityrnaseq 00_scripts/trinity_utils
 
-
 # clone trinotate
 git clone https://github.com/Trinotate/Trinotate 00_scripts/trinotate_utils
+
+#clone transDecoder
+git clone https://github.com/TransDecoder/TransDecoder 00_scripts/transdecoder_utils
+
+# clone transvestigator
+git clone https://github.com/genomeannotation/transvestigator 00_scripts/transvestigator_utils
+
+#clone transrate
+git clone https://github.com/blahah/transrate 00_scripts/transrate_utils
