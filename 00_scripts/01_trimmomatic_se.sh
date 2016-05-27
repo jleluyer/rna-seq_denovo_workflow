4fe3a1431bdb18bafb96a32458079058d8923565
jleluyer <jeremy.le.luyer@gmail.com>
2016-05-27 14:20:22 -0400

    adapted for slurm

--- 00_scripts/01_trimmomatic_se.sh
+++ 00_scripts/01_trimmomatic_se.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#$ -N trimmomatic_se
-#$ -M userID
-#$ -m beas
-#$ -pe smp 8
-#$ -l h_vmem=20G
-#$ -l h_rt=20:00:00
-#$ -cwd
-#$ -S /bin/bash
+
+#SBATCH -D ./ 
+#SBATCH --job-name="trim"
+#SBATCH -o log-trim_pe.out
+#SBATCH -c 8
+#SBATCH -p ibismini
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=type_your_mail@ulaval.ca
+#SBATCH --time=0-20:00
+#SBATCH --mem=50000
+
+cd $SLURM_SUBMIT_DIR
 
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 SCRIPT=$0
@@ -19,15 +23,13 @@ cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 ADAPTERFILE="/path/to/file.fasta"
 TRIMMOMATIC_JAR="/prg/trimmomatic/0.36/trimmomatic-0.36.jar"
 
-# Move to job submission directory
-cd $SGE_O_WORKDIR
-
 for file in $(ls 02_data/*.f*q.gz|perl -pe 's/_R[12].f(ast)?q.gz//')
 do
         base=$(basename "$file")
 
 java -Xmx40G -jar $TRIMMOMATIC_JAR SE \
         -phred33 \
+        -threads 8 \
         02_data/"$base"_R1.fastq.gz \
         03_trimmed/"$base"_R1.trimmed.fastq.gz \
         ILLUMINACLIP:"$ADAPTERFILE":2:20:7 \
