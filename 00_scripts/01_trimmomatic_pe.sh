97e930db0088ad44bf2b6ee76ad776eba8b098f4
jleluyer <jeremy.le.luyer@gmail.com>
2016-06-01 11:38:19 -0400

    changed trim pe

--- 00_scripts/01_trimmomatic_pe.sh
+++ 00_scripts/01_trimmomatic_pe.sh
@@ -20,7 +20,7 @@ cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
 
 #global variables
-ADAPTERFILE="/rap/userID/00_ressources/02_databases/univec/univec.fasta"
+ADAPTERFILE="01_info_files/univec.fasta"
 TRIMMOMATIC_JAR="/prg/trimmomatic/0.36/trimmomatic-0.36.jar"
 
 for file in $(ls 02_data/*.f*q.gz|perl -pe 's/_R[12].f(ast)?q.gz//')
@@ -30,8 +30,8 @@ do
 java -Xmx40G -jar $TRIMMOMATIC_JAR PE \
         -phred33 \
 	-threads 8 \
-        02_data/"$base"_R1.fastq.gz \
-        02_data/"$base"_R2.fastq.gz \
+        02_data/"$base"_1.fq.gz \
+        02_data/"$base"_2.fq.gz \
         03_trimmed/"$base"_R1.paired.fastq.gz \
         03_trimmed/"$base"_R1.single.fastq.gz \
         03_trimmed/"$base"_R2.paired.fastq.gz \
