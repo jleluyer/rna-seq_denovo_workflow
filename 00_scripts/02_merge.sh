b5976289da366b9fb995eed7061039c204de8c38
jleluyer <jeremy.le.luyer@gmail.com>
2016-06-01 12:40:13 -0400

    Update 02_merge.sh

--- 00_scripts/02_merge.sh
+++ 00_scripts/02_merge.sh
@@ -4,16 +4,9 @@ INPUTFOLDER="03_trimmed"
 OUTPUTFOLDER="04_merged"
 
 #move to present directory
-
 cd $(pwd)
 
-for file in $(ls 02_data/*.f*q.gz|perl -pe 's/_R[12].f(ast)?q.gz//')
-do
-	base=$(basename "$file")
-
-	cat "$INPUTFOLDER"/"$base"_R1.paired.fastq.gz > all_reads.left.fastq.gz
+#cat all reads
+	cat "$INPUTFOLDER"/*_R1.paired.fastq.gz > all_reads.left.fastq.gz
  
-	cat "$INPUTFOLDER"/"$base"_R2.paired.fastq.gz > all_reads.left.fastq.gz
-
-
-done  2>&1 | tee 98_log_files/"$TIMESTAMP"_mere.log        
+	cat "$INPUTFOLDER"/*_R2.paired.fastq.gz > all_reads.right.fastq.gz
