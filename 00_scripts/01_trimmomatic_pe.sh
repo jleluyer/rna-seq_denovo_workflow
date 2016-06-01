49be8c9bdce3b6bb63e122ff0d8069364b254ed5
jleluyer <jeremy.le.luyer@gmail.com>
2016-06-01 11:34:39 -0400

    Update 01_trimmomatic_pe.sh

--- 00_scripts/01_trimmomatic_pe.sh
+++ 00_scripts/01_trimmomatic_pe.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#$ -N trimmomatic
-#$ -M userID
-#$ -m beas
-#$ -pe smp 8
-#$ -l h_vmem=20G
-#$ -l h_rt=20:00:00
-#$ -cwd
-#$ -S /bin/bash
+
+#SBATCH -D ./ 
+#SBATCH --job-name="trim"
+#SBATCH -o log-trim_pe.out
+#SBATCH -c 8
+#SBATCH -p ibismini
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=type_your_mail@ulaval.ca
+#SBATCH --time=0-20:00
+#SBATCH --mem=50000
+
+cd $SLURM_SUBMIT_DIR
 
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 SCRIPT=$0
@@ -18,8 +22,6 @@ cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 #global variables
 ADAPTERFILE="/rap/userID/00_ressources/02_databases/univec/univec.fasta"
 TRIMMOMATIC_JAR="/prg/trimmomatic/0.36/trimmomatic-0.36.jar"
-# Move to job submission directory
-cd $SGE_O_WORKDIR
 
 for file in $(ls 02_data/*.f*q.gz|perl -pe 's/_R[12].f(ast)?q.gz//')
 do
