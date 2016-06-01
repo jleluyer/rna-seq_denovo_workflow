7878c1df7c6b5afebfdd025921f65efc71fdaaba
jleluyer <jeremy.le.luyer@gmail.com>
2016-06-01 12:41:05 -0400

    Update 02_merge.sh

--- 00_scripts/02_merge.sh
+++ 00_scripts/02_merge.sh
@@ -7,6 +7,6 @@ OUTPUTFOLDER="04_merged"
 cd $(pwd)
 
 #cat all reads
-	cat "$INPUTFOLDER"/*_R1.paired.fastq.gz > all_reads.left.fastq.gz
+	cat "$INPUTFOLDER"/*_R1.paired.fastq.gz > "$OUTPUTFOLDER"/all_reads.left.fastq.gz
  
-	cat "$INPUTFOLDER"/*_R2.paired.fastq.gz > all_reads.right.fastq.gz
+	cat "$INPUTFOLDER"/*_R2.paired.fastq.gz > "$OUTPUTFOLDER"/all_reads.right.fastq.gz
