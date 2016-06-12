9dfc79ffa97317b27e73ac30da82099fc99d9162
jleluyer <jeremy.le.luyer@gmail.com>
2016-06-12 14:57:09 -0400

    Update 06_transcripts_abundance.sh

--- 00_scripts/06_transcripts_abundance.sh
+++ 00_scripts/06_transcripts_abundance.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-#$ -N aboudance_stats
-#$ -M userID
-#$ -m beas
-#$ -pe smp 8
-#$ -l h_vmem=60G
-#$ -l h_rt=20:00:00
-#$ -cwd
-#$ -S /bin/bash
+
+#SBATCH -D ./ 
+#SBATCH --job-name="trans_abundance"
+#SBATCH -o log-trans_abundance.out
+#SBATCH -c 8
+#SBATCH -p ibismini
+#SBATCH -A ibismini
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=type_your_mail@ulaval.ca
+#SBATCH --time=01-00:00
+#SBATCH --mem=50000
+
+cd $SLURM_SUBMIT_DIR
 
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 SCRIPT=$0
@@ -14,8 +19,6 @@ NAME=$(basename $0)
 LOG_FOLDER="98_log_files"
 cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
-#Move to job submission directory
-cd $SGE_O_WORKDIR
 
 for file in $(ls 03_trimmed/*.paired.f*q.gz|perl -pe 's/_R[12].fastq.gz//')
 
