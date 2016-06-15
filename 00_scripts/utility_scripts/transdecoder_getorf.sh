66afe46205314a36dda0ff1aa8ec058e73a5e88f
jleluyer <jeremy.le.luyer@gmail.com>
2016-06-15 12:59:23 -0400

    changed transdecoder script

--- 00_scripts/utility_scripts/transdecoder_getorf.sh
+++ 00_scripts/utility_scripts/transdecoder_getorf.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-#$ -N trimmomatic
-#$ -M userID
-#$ -m beas
-#$ -pe smp 8
-#$ -l h_vmem=20G
-#$ -l h_rt=20:00:00
-#$ -cwd
-#$ -S /bin/bash
+
+#SBATCH -D ./ 
+#SBATCH --job-name="transdecoder"
+#SBATCH -o log-transdecoder.out
+#SBATCH -c 1
+#SBATCH -p ibismini
+#SBATCH -A ibismini
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=type_your_mail@ulaval.ca
+#SBATCH --time=5-00:00
+#SBATCH --mem=50000
 
 TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
 SCRIPT=$0
@@ -14,10 +17,7 @@ NAME=$(basename $0)
 LOG_FOLDER="98_log_files"
 cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
 
-# Move to job submission directory
-cd $SGE_O_WORKDIR
-
 #Global variables
-INPUT="05_trinity_assembly/Trinity.fasta"
+INPUT="05_trinity_assembly/Trinity_cleaned.fasta"
 
 ./00_scripts/transdecoder_utils/TransDecoder.LongOrfs -t $INPUT 2>&1 | tee 98_log_files/"$TIMESTAMP"_transdecoder_getorf.log   
